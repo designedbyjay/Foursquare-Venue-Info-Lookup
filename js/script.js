7c34c10961879aa5658fa9c6dcdba2d0839f2184
Chad <chietala@umflint.edu>
2011-02-15 08:54:15 -0500

    Added css

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ Chad Hietala
 */
 
 
- function fsq(venue, callback){
+function fsq(venue, callback){
 	
 	if (!venue){
 		alert('No venue was passed.');
@@ -14,7 +14,6 @@ Chad Hietala
 
 	$.getJSON(yql, cbFunc);
 	
-	
 	function cbFunc(data){
 		if(!data.query.results.venue.stats.mayor){
 			alert('No Data sorry');
@@ -23,47 +22,40 @@ Chad Hietala
 		if (typeof callback === 'function'){
 			callback (
 			'<ul>'
-			+ '<li> <strong>Total Check Ins:</strong> ' + data.query.results.venue.stats.checkins + '</li>'
+			+ '<li> <strong>Total Check Ins:</strong> <span class="count">' + data.query.results.venue.stats.checkins + '</span></li>'
 			+ '<li><strong>Current Mayor</strong> <div class="media mayor"><div class="img"><img width="50" height="50" src="' + data.query.results.venue.stats.mayor.user.photo + '" /></div><div class="bd">' 
 			 + '<h2>'+ data.query.results.venue.stats.mayor.user.firstname + '&nbsp;' + data.query.results.venue.stats.mayor.user.lastname + '</h2>' + '</div><div class="count">' + data.query.results.venue.stats.mayor.count + '<span class="label"> Checkins</span></div></div></li>'
 
 			+ '</ul>');
 		}
-		
-
 	};
-
 }
 
-
 $(document).ready(function() {
-
-	
 	$('#venues a').click(function(){
+		event.preventDefault();
 		var path = $(this).attr('data-venue');
 		var result = $(this).next('div');
 		
-		// check to see if result is hidden if it is 
+		// check to see if the trough is in the pen or not
 		if(result.is(":hidden")){
-			
-		
 		 fsq(path, function(results){
-
-					// check to see if there are already results
+					// check to see if this pig is empty
 					if(result.is(":empty")){
+						// Fill it with jSON slop YUM!
 						result.append(results);
 					}
-
 		});
-		
-		result.delay(100).slideDown();
+			// slide in the trough into the pen
+			result.delay(100).slideDown();
 	 	}else
-	{
-		result.slideUp();
-	}
+		{
+			// otherwise take the trough away
+			result.slideUp();
+		}
+	});
 	
-	
-});	
+		
 	
 });
 
